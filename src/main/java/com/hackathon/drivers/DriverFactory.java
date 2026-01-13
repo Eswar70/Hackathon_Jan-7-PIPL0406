861742af151a7520cba18abb9fba90bcd387dcfe
Eswar Reddy Narreddy <125456208+Eswar70@users.noreply.github.com>
2026-01-13 14:09:00 +0530

    Update DriverFactory.java

--- src/main/java/com/hackathon/drivers/DriverFactory.java
+++ src/main/java/com/hackathon/drivers/DriverFactory.java
@@ -11,12 +11,10 @@ public class DriverFactory {
 
     public static WebDriver initDriver() {
     	EdgeOptions options = new EdgeOptions();
-        if (System.getProperty("headless", "false").equals("true")) { 
-            options.addArguments("--headless"); 
-            options.addArguments("--disable-gpu"); 
-            options.addArguments("--no-sandbox"); 
-            options.addArguments("--disable-dev-shm-usage"); 
-        }
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         options.setExperimentalOption("excludeSwitches", new String[] {"enable-automation"});
         options.addArguments("--start-maximized");
         driver.set(new EdgeDriver(options));
